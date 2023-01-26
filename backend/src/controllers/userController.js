5ceabdec9fedd88f6554801d2d1512741322610c
stocaline <stocaline2@gmail.com>
2023-01-26 12:55:36 -0300

    Correção no login, erro quando o usuário não existe

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,7 +2,8 @@ const userModel = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { json } = require('express');
-const SECRET = 'apicepsi';
+require('dotenv').config();
+const SECRET = process.env.SECRET;
 
 const getAll = async (_req, res) => {
   const users = await userModel.getAll();
@@ -21,20 +22,23 @@ const loginUser = async (req, res) => {
   if (!matricula || !password) {
     res.status(422).send('Você deve definir matricula e senha!');
   }
-  
-  try{
+
+  try {
     const user = await userModel.loginUser(matricula);
-    const passwordMath = await bcrypt.compare(password, user.password);
-    
-    if(passwordMath) {
-      const token = jwt.sign({ userId: 1 }, SECRET, { expiresIn: 3000 });
-      const id = user.id;
-      const name = user.name;
-      return res.json({ id, name, matricula, auth: true, token });
+    if (user) {
+      console.log(password)
+      const passwordMath = await bcrypt.compare(password, user.password);
+
+      if (passwordMath) {
+        const token = jwt.sign({ userId: 1 }, SECRET, { expiresIn: 3000 });
+        const id = user.id;
+        const name = user.name;
+        return res.json({ id, name, matricula, auth: true, token });
+      }
     }
     return res.status(401).end();
 
-  }catch(erro){
+  } catch (erro) {
     return console.error("Login User", erro);
   }
 };
