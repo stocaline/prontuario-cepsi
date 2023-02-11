460418c11d511e031bb04822b2451dfea927e9da
stocaline <stocaline2@gmail.com>
2023-02-11 17:13:36 -0300

    Cadastrando usuario

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 
-const router = Router();
+import { CreateUserController } from './controllers/user/CreateUserController';
 
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ nome: 'teste' })
-})
+const router = Router()
 
-export { router };
+router.post('/users', new CreateUserController().handle)
+
+export { router }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,11 +4,11 @@ import cors from 'cors';
 import { router } from './routes';
 
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 app.use(cors())
 
-app.use(router);
+app.use(router)
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if(err instanceof Error){
