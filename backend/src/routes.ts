d24cf96ae8e5390b150538aa2f0f42ea6af7a5e2
stocaline <stocaline2@gmail.com>
2023-02-12 17:38:13 -0300

    Criando rota de informações do usuario e middleware para rotas privadas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
-
+import { DetailUserController } from './controllers/user/DetailUsercontroller';
+import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const router = Router()
+
+router.get('/user/info', isAuthenticated, new DetailUserController().handle)
 
 router.post('/users', new CreateUserController().handle)
 
--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -33,7 +33,7 @@ class AuthUserService{
             process.env.JWT_SECRET,
             {
                 subject: user.id,
-                expiresIn: '1d'
+                expiresIn: '10d'
             }
 
         )
