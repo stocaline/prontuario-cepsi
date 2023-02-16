d0a56fd7444816ed33bbc9f3393206773286db13
stocaline <stocaline2@gmail.com>
2023-02-16 16:08:33 -0300

    Pequenos ajustes de rota

--- backend/src/services/user/DetailUserService.ts
+++ backend/src/services/user/DetailUserService.ts
@@ -11,6 +11,7 @@ class DetailUserService{
                 email: true,
                 admin: true,
                 registration: true,
+                patients: true,
             }
         })
 
