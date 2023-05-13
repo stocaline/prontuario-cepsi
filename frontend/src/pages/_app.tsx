f83a2460c2ec0b01d948ebe0ff5627264e6c5677
stocaline <stocaline2@gmail.com>
2023-05-13 14:50:43 -0300

    adição de barra de loading

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,8 +2,14 @@ import '../../styles/global.scss'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 import type { AppProps } from 'next/app'
+import Router from "next/router"
+import NProgress from "nprogress"
 
 import { AuthProvider } from '../contexts/AuthContext'
+
+Router.events.on("routeChangeStart", (url) => {NProgress.start()})
+Router.events.on("routeChangeComplete", () => NProgress.done())
+Router.events.on("routeChangeError", () => NProgress.done())
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
